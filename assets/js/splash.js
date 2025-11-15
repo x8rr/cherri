0526b8944be2b2884e16e1e3605a31fafe22e82f
x8r <channelediting521@gmail.com>
2025-11-15 12:34:09 -0600

    overhaul

---
+++ assets/js/splash.js
@@ -0,0 +1,18 @@
+const splashes = [
+    "wsp",
+    "made by x8r",
+    "im hungry",
+    "w speed",
+    "scramjet > ultraviolet",
+    "social stuff coming soon",
+    "penguins :thumbs-up:",
+    "plz star",
+    "sock",
+    "sdakjlfhaskdfg"
+]
+
+const randomSplashN = Math.floor(Math.random() * splashes.length)
+const randomSplash = splashes[randomSplashN]
+
+const splashTextEl = document.getElementById("splash")
+splashTextEl.textContent = randomSplash
